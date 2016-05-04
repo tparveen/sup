074d9b2f3bd708fb6775fb46cf494f51d455e2b7
Joe Turner <joe@oampo.co.uk>
2016-05-04 10:51:57 +0100

    Listen on env port.

--- index.js
+++ index.js
@@ -249,8 +249,9 @@ app.get('/messages/:messageId', function(req, res) {
 var runServer = function(callback) {
     var databaseUri = process.env.DATABASE_URI || global.databaseUri || 'mongodb://localhost/sup';
     mongoose.connect(databaseUri).then(function() {
-        var server = app.listen(8080, function() {
-            console.log('Listening on localhost:8080');
+        var port = process.env.PORT || 8080;
+        var server = app.listen(port, function() {
+            console.log('Listening on localhost:' + port);
             if (callback) {
                 callback(server);
             }
